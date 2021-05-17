6707d8ad19d940ae0a86b98822c3ec9d7d5c9de5
samuelalmeida95 <samuelalmeida95>
2021-05-17 18:11:08 -0300

    start project give me back api

---
+++ src/main/java/projects/givemebackapi/GivemebackApiApplication.java
@@ -0,0 +1,13 @@
+package projects.givemebackapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GivemebackApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GivemebackApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projects/givemebackapi/GivemebackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package projects.givemebackapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GivemebackApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
