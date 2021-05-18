77cfe2983f4cdf0f0f245ded2713a0d88972871d
samuelalmeida95 <samuelalmeida95>
2021-05-18 12:29:43 -0300

    implementação inicial para entidade ItemEmprestado e criando o relacionamento entre ela e a entidade DonoItem

--- src/main/java/projects/givemebackapi/model/ItemEmprestado.java
+++ src/main/java/projects/givemebackapi/model/ItemEmprestado.java
@@ -1,5 +1,43 @@
 package projects.givemebackapi.model;
 
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Data;
+
+@Data
+@Entity
 public class ItemEmprestado {
 
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idItem;
+    
+    private String nomeItem;
+    private String descricaoItem;
+    private Date dataDevolucaoItem;
+    
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name="donoItem_id")
+    private DonoItem donoItem;
+    
+    
+    public ItemEmprestado(Integer idItem, String nomeItem, String descricaoItem, DonoItem donoItem) {
+        this.idItem = idItem;
+        this.nomeItem = nomeItem;
+        this.descricaoItem = descricaoItem;
+        this.donoItem = donoItem;
+    }
+
+    public ItemEmprestado(){}
 }
