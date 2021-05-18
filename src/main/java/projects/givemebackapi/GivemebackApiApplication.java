d9a43199974de4a1310c15e11a044b198f97dd3b
samuelalmeida95 <samuelalmeida95>
2021-05-18 13:52:24 -0300

    configurando start da base de dados automatica

--- src/main/java/projects/givemebackapi/GivemebackApiApplication.java
+++ src/main/java/projects/givemebackapi/GivemebackApiApplication.java
@@ -1,7 +1,12 @@
 package projects.givemebackapi;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import projects.givemebackapi.services.DBService;
 
 @SpringBootApplication
 public class GivemebackApiApplication {
@@ -9,5 +14,19 @@ public class GivemebackApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GivemebackApiApplication.class, args);
 	}
+
+	@Autowired
+    private DBService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
+    @Bean
+    public boolean instanciaBaseDeDados() {
+        if (strategy.equals("create")) {
+            this.dbService.instanciaBaseDeDados();
+        }
+        return false;
+    }
 
 }
