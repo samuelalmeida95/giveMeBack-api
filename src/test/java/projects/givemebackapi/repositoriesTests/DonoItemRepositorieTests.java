afd23d7b7fcd06722ae5d376a1548c9365e698fe
samuelalmeida95 <samuelalmeida95>
2021-05-18 14:04:02 -0300

    iniciando classe de testes para entidade DonoItem Repository

---
+++ src/test/java/projects/givemebackapi/repositoriesTests/DonoItemRepositorieTests.java
@@ -0,0 +1,20 @@
+package projects.givemebackapi.repositoriesTests;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import projects.givemebackapi.repositories.DonoItemRepository;
+
+@DataJpaTest
+public class DonoItemRepositorieTests {
+
+
+    private DonoItemRepository donoItemRepository;
+
+    void salvar_PersistirDonoItemQuandoBemSucedido() {
+
+    }
+    
+}
+
+    
+
