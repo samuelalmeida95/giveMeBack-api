0dc59f9b96579f1ce8e474b71cbeef785b425844
samuelalmeida95 <samuelalmeida95>
2021-05-18 15:09:45 -0300

    Beans Validation para entidade DonoItem

--- src/main/java/projects/givemebackapi/model/DonoItem.java
+++ src/main/java/projects/givemebackapi/model/DonoItem.java
@@ -8,6 +8,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
 
 import lombok.Data;
 
@@ -18,7 +21,15 @@ public class DonoItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idDono;
+
+    
+    @NotEmpty(message = "Campo NOME DONO é obrigatório!") 
+    @Length(min = 3, max = 100, message = "Campo NOME DONO deve ter entre 3 e 100 caracteres")
     private String nomeDono;
+
+    
+    @NotEmpty(message = "Campo WHATSAPP é obrigatório!") 
+    @Length(min = 3, max = 50, message = "Campo WHATSAPP deve ter entre 3 e 100 caracteres")
     private String whatsappDono;
 
     @OneToMany(mappedBy = "donoItem")  //um dono para muitos itens
