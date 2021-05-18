3f8d37e0c7e105946bcdf7809b83f2a8304f232d
samuelalmeida95 <samuelalmeida95>
2021-05-18 14:53:47 -0300

    alterando instância do DBService para o perfil de Dev

--- src/main/java/projects/givemebackapi/GivemebackApiApplication.java
+++ src/main/java/projects/givemebackapi/GivemebackApiApplication.java
@@ -1,12 +1,7 @@
 package projects.givemebackapi;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import projects.givemebackapi.services.DBService;
 
 @SpringBootApplication
 public class GivemebackApiApplication {
@@ -14,19 +9,5 @@ public class GivemebackApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GivemebackApiApplication.class, args);
 	}
-
-	@Autowired
-    private DBService dbService;
-
-    @Value("${spring.jpa.hibernate.ddl-auto}")
-    private String strategy;
-
-    @Bean
-    public boolean instanciaBaseDeDados() {
-        if (strategy.equals("create")) {
-            this.dbService.instanciaBaseDeDados();
-        }
-        return false;
-    }
 
 }
--- src/main/java/projects/givemebackapi/services/DonoItemService.java
+++ src/main/java/projects/givemebackapi/services/DonoItemService.java
@@ -1,5 +1,8 @@
 package projects.givemebackapi.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class DonoItemService {
     
 }
