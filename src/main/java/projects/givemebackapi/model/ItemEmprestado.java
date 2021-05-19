db330348dfd9d71599f0b2688d06842be91645d9
samuelalmeida95 <samuelalmeida95>
2021-05-18 21:37:31 -0300

    adicionando Beans validation para entidade ItemEmprestado

--- src/main/java/projects/givemebackapi/model/ItemEmprestado.java
+++ src/main/java/projects/givemebackapi/model/ItemEmprestado.java
@@ -8,8 +8,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotEmpty;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import org.hibernate.validator.constraints.Length;
 
 import lombok.Data;
 
@@ -22,8 +25,14 @@ public class ItemEmprestado {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idItem;
     
+    @NotEmpty(message = "Campo NOME ITEM é obrigatório!") 
+    @Length(min = 3, max = 100, message = "Campo NOME ITEM deve ter entre 3 e 100 caracteres")
     private String nomeItem;
+
+    @NotEmpty(message = "Campo DESCRIÇÃO é obrigatório!") 
+    @Length(min = 3, max = 100, message = "Campo DESCRIÇÃO deve ter entre 3 e 100 caracteres")
     private String descricaoItem;
+    
     private Date dataDevolucaoItem;
     
     @JsonIgnore
