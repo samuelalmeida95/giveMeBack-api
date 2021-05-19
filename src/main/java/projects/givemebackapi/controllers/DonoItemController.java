6cea5fab7492e6b77b18f0d39db3663d44de9a92
samuelalmeida95 <samuelalmeida95>
2021-05-19 11:30:03 -0300

    GET por id para um Dono Item

--- src/main/java/projects/givemebackapi/controllers/DonoItemController.java
+++ src/main/java/projects/givemebackapi/controllers/DonoItemController.java
@@ -1,5 +1,26 @@
 package projects.givemebackapi.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import projects.givemebackapi.model.DonoItem;
+import projects.givemebackapi.services.DonoItemService;
+
+@RestController
+@RequestMapping(value = "/donos")
 public class DonoItemController {
     
+    @Autowired
+    private DonoItemService donoItemService;
+
+
+    @GetMapping(value = "/{idDonoItem}")
+    public ResponseEntity<DonoItem> findById(@PathVariable Integer idDonoItem) {
+        DonoItem dono = donoItemService.findById(idDonoItem);
+        return ResponseEntity.ok(dono);
+    }
 }
--- src/main/java/projects/givemebackapi/services/DonoItemService.java
+++ src/main/java/projects/givemebackapi/services/DonoItemService.java
@@ -1,8 +1,24 @@
 package projects.givemebackapi.services;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import projects.givemebackapi.model.DonoItem;
+import projects.givemebackapi.repositories.DonoItemRepository;
 
 @Service
 public class DonoItemService {
     
+
+    @Autowired
+    private DonoItemRepository donoItemRepository;
+
+    public DonoItem findById(Integer idDonoItem) {
+        Optional<DonoItem> donoItem = donoItemRepository.findById(idDonoItem);
+        return donoItem.orElseThrow(() -> new RuntimeException(
+            "Dono de item não encontrado! " + idDonoItem + " Tipo: " + DonoItem.class.getName()));
+        
+    }
 }
