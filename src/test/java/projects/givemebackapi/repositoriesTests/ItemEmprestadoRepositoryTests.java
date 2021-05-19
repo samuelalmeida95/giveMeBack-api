24d39dd3e4e51bd7f4eb82ce491aff60ee6c672c
samuelalmeida95 <samuelalmeida95>
2021-05-18 21:21:58 -0300

    TESTE criando um novo ItemEmprestado

--- src/test/java/projects/givemebackapi/repositoriesTests/ItemEmprestadoRepositoryTests.java
+++ src/test/java/projects/givemebackapi/repositoriesTests/ItemEmprestadoRepositoryTests.java
@@ -1,10 +1,16 @@
 package projects.givemebackapi.repositoriesTests;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import projects.givemebackapi.model.ItemEmprestado;
 import projects.givemebackapi.repositories.ItemEmprestadoRepository;
+import projects.givemebackapi.util.ItemEmprestadoCreator;
+
+
 
 @DataJpaTest
 @DisplayName("Testes para Repositorio de ItemEmprestado")
@@ -12,5 +18,19 @@ public class ItemEmprestadoRepositoryTests {
 
     @Autowired
     private ItemEmprestadoRepository itemEmprestadoRepository;
+
+
+    @Test
+    @DisplayName("Salvar e persistir ItemEmprestado quando bem sucedido")
+    void salvar_PersistirItemEmprestadoQuandoBemSucedido() {
+        ItemEmprestado itemEmprestadoParaSalvar = ItemEmprestadoCreator.criarItemEmprestado();
+        ItemEmprestado itemEmprestadoSalvo = this.itemEmprestadoRepository.save(itemEmprestadoParaSalvar);
+
+        Assertions.assertThat(itemEmprestadoSalvo).isNotNull();
+        Assertions.assertThat(itemEmprestadoSalvo.getIdItem()).isNotNull();
+        Assertions.assertThat(itemEmprestadoSalvo.getNomeItem()).isNotNull();
+        Assertions.assertThat(itemEmprestadoSalvo.getDescricaoItem()).isNotNull();
+        Assertions.assertThat(itemEmprestadoSalvo.getNomeItem()).isEqualTo(itemEmprestadoParaSalvar.getNomeItem());
+    }
     
 }
