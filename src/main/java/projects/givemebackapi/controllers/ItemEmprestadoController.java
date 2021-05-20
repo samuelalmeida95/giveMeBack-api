3b0630f2a9d89b5bc56910686de081cb11212058
samuelalmeida95 <samuelalmeida95>
2021-05-20 10:31:07 -0300

    adicionando funcionalidade de um itemEmprestado ter seu Enum setado como padrão para EMPRESTADO, fora isso iniciando Controller para essa entidade.

--- src/main/java/projects/givemebackapi/controllers/ItemEmprestadoController.java
+++ src/main/java/projects/givemebackapi/controllers/ItemEmprestadoController.java
@@ -1,5 +1,10 @@
 package projects.givemebackapi.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/itens")
 public class ItemEmprestadoController {
     
 }
--- src/main/java/projects/givemebackapi/services/DBService.java
+++ src/main/java/projects/givemebackapi/services/DBService.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Service;
 import projects.givemebackapi.model.AmigoEmprestimo;
 import projects.givemebackapi.model.DonoItem;
 import projects.givemebackapi.model.ItemEmprestado;
-import projects.givemebackapi.model.TipoStatus;
 import projects.givemebackapi.repositories.AmigoEmprestimoRepository;
 import projects.givemebackapi.repositories.DonoItemRepository;
 import projects.givemebackapi.repositories.ItemEmprestadoRepository;
@@ -30,20 +29,20 @@ public class DBService {
     public void instanciaBaseDeDados(){
 
         DonoItem donoItem = new DonoItem(null, "Samuel Almeida", "555-777-522");
-        DonoItem donoItem1 = new DonoItem(null, "Ragnar", "666-999-323");
-        DonoItem donoItem2 = new DonoItem(null, "Ronaldinha", "112-346-978");
+        DonoItem donoItem1 = new DonoItem(null, "Ellon Musk", "666-999-323");
+        DonoItem donoItem2 = new DonoItem(null, "Steve Jobs", "112-346-978");
     
-        AmigoEmprestimo amigoEmprestimo = new AmigoEmprestimo(null, "José", "987", "rua x");
-        AmigoEmprestimo amigoEmprestimo1 = new AmigoEmprestimo(null, "pedro", "987", "rua x");
-        AmigoEmprestimo amigoEmprestimo2 = new AmigoEmprestimo(null, "joao", "987", "rua x");
+        AmigoEmprestimo amigoEmprestimo = new AmigoEmprestimo(null, "José", "987", "rua do Jose");
+        AmigoEmprestimo amigoEmprestimo1 = new AmigoEmprestimo(null, "Pedro", "987", "rua do Pedro");
+        AmigoEmprestimo amigoEmprestimo2 = new AmigoEmprestimo(null, "João", "987", "avenida do João");
 
-        ItemEmprestado itemEmprestado = new ItemEmprestado(null, "Video Game", "um video game",TipoStatus.EMPRESTADO, donoItem ,LocalDate.of(2022, 2, 15), amigoEmprestimo);
-        ItemEmprestado itemEmprestado1 = new ItemEmprestado(null, "Relogio", "um relogio",TipoStatus.EMPRESTADO, donoItem2 ,LocalDate.of(2022, 2, 15), amigoEmprestimo1);
-        ItemEmprestado itemEmprestado2 = new ItemEmprestado(null, "tenis", "um tenis",TipoStatus.EMPRESTADO, donoItem1 ,LocalDate.of(2022, 2, 15), amigoEmprestimo2);
+        ItemEmprestado itemEmprestado = new ItemEmprestado(null, "video game", "um video game", donoItem ,LocalDate.of(2022, 2, 15), amigoEmprestimo);
+        ItemEmprestado itemEmprestado1 = new ItemEmprestado(null, "relogio", "um relogio", donoItem2 ,LocalDate.of(2022, 2, 15), amigoEmprestimo1);
+        ItemEmprestado itemEmprestado2 = new ItemEmprestado(null, "tenis", "um tenis", donoItem1 ,LocalDate.of(2022, 2, 15), amigoEmprestimo2);
 
-        ItemEmprestado itemEmprestado3 = new ItemEmprestado(null, "bone", "um bone",TipoStatus.EMPRESTADO, donoItem1 ,LocalDate.of(2022, 2, 15), amigoEmprestimo1);
-        ItemEmprestado itemEmprestado4 = new ItemEmprestado(null, "pipa", "uma pipa",TipoStatus.EMPRESTADO, donoItem1 ,LocalDate.of(2022, 2, 15), amigoEmprestimo2);
-        ItemEmprestado itemEmprestado5 = new ItemEmprestado(null, "caneta", "uma caneta",TipoStatus.EMPRESTADO, donoItem1 ,LocalDate.of(2022, 2, 15), amigoEmprestimo2);
+        ItemEmprestado itemEmprestado3 = new ItemEmprestado(null, "bone", "um bone", donoItem1 ,LocalDate.of(2022, 2, 15), amigoEmprestimo1);
+        ItemEmprestado itemEmprestado4 = new ItemEmprestado(null, "pipa", "uma pipa", donoItem1 ,LocalDate.of(2022, 2, 15), amigoEmprestimo2);
+        ItemEmprestado itemEmprestado5 = new ItemEmprestado(null, "caneta", "uma caneta", donoItem1 ,LocalDate.of(2022, 2, 15), amigoEmprestimo2);
 
 
         donoItem.getItensEmprestados().addAll(Arrays.asList(itemEmprestado, itemEmprestado1, itemEmprestado2));
