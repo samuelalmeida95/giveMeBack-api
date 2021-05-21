12390c6e97109dfc0e6e6577d7d20cb095967d99
samuelalmeida95 <samuelalmeida95>
2021-05-21 19:40:35 -0300

    implementação do DonoItemDTO para seu controller

--- src/main/java/projects/givemebackapi/controllers/DonoItemController.java
+++ src/main/java/projects/givemebackapi/controllers/DonoItemController.java
@@ -1,9 +1,10 @@
 package projects.givemebackapi.controllers;
 
-import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,55 +13,58 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import projects.givemebackapi.dtos.DonoItemDTO;
 import projects.givemebackapi.model.DonoItem;
 import projects.givemebackapi.services.DonoItemService;
 
 @RestController
 @RequestMapping(value = "/donos")
 public class DonoItemController {
-    
+
     @Autowired
     private DonoItemService donoItemService;
 
-
     @GetMapping(value = "/{idDonoItem}")
-    public ResponseEntity<DonoItem> findById(@PathVariable Integer idDonoItem) {
+    public ResponseEntity<DonoItemDTO> findById(@PathVariable Integer idDonoItem) {
         DonoItem donoItem = donoItemService.findById(idDonoItem);
-        return ResponseEntity.ok(donoItem);
+        DonoItemDTO donoDTO = new DonoItemDTO(donoItem);
+
+        return ResponseEntity.ok(donoDTO);
     }
 
     @GetMapping(value = "/search")
-    public ResponseEntity<DonoItem> findByNomeDono(@RequestParam String nomeDono) {
+    public ResponseEntity<DonoItemDTO> findByNomeDono(@RequestParam String nomeDono) {
         DonoItem donoItem = donoItemService.findByNomeDono(nomeDono);
-        return ResponseEntity.ok().body(donoItem);
+        DonoItemDTO donoDTO = new DonoItemDTO(donoItem);
+
+        return ResponseEntity.ok().body(donoDTO);
     }
 
     @GetMapping(value = "/list")
-    public ResponseEntity<List<DonoItem>> findAll() {
+    public ResponseEntity<List<DonoItemDTO>> findAll() {
         List<DonoItem> listDonos = donoItemService.findAll();
-        return ResponseEntity.ok().body(listDonos);
+        List<DonoItemDTO> listDTO = listDonos.stream().map(dono -> new DonoItemDTO(dono)).collect(Collectors.toList());
+
+        return ResponseEntity.ok().body(listDTO);
     }
 
     @PostMapping
-    public ResponseEntity<DonoItem> createNewDono(@RequestBody DonoItem dono) {
-
+    @ResponseStatus(HttpStatus.CREATED)
+    public DonoItemDTO createNewDono(@RequestBody DonoItem dono) {
         DonoItem novoDonoItem = donoItemService.createNewDono(dono);
+        DonoItemDTO donoDTO = new DonoItemDTO(novoDonoItem);
 
-        URI uri = ServletUriComponentsBuilder
-        .fromCurrentRequest()
-        .path("/{id}")
-        .buildAndExpand(novoDonoItem.getId())
-        .toUri();
-
-        return ResponseEntity.created(uri).build();
+        return donoDTO;
     }
 
     @PutMapping(value = "/{id}")
-    public ResponseEntity<DonoItem> update(@PathVariable Integer id, @RequestBody DonoItem dono) {
+    public ResponseEntity<DonoItemDTO> update(@PathVariable Integer id, @RequestBody DonoItem dono) {
         DonoItem novoDonoItem = donoItemService.updateDono(id, dono);
-        return ResponseEntity.ok().body(novoDonoItem);
+        DonoItemDTO donoDTO = new DonoItemDTO(novoDonoItem);
+
+        return ResponseEntity.ok().body(donoDTO);
     }
 }
