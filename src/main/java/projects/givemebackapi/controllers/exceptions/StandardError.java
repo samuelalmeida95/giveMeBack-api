11006aebd9fde009adce9d7a0078d94e7ae3357b
samuelalmeida95 <samuelalmeida95>
2021-05-22 18:29:47 -0300

    refatorando Classes ResourceExceptionHandler e StandardError, procurando deixar a visualizações dos métodos mais clean possivel.

--- src/main/java/projects/givemebackapi/controllers/exceptions/StandardError.java
+++ src/main/java/projects/givemebackapi/controllers/exceptions/StandardError.java
@@ -1,5 +1,8 @@
 package projects.givemebackapi.controllers.exceptions;
 
+import lombok.Data;
+
+@Data
 public class StandardError {
 
     private Long timestamp;
@@ -13,30 +16,6 @@ public class StandardError {
     public StandardError(Long timestamp, Integer status, String message) {
         this.timestamp = timestamp;
         this.status = status;
-        this.message = message;
-    }
-
-    public Long getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(Long timestamp) {
-        this.timestamp = timestamp;
-    }
-
-    public Integer getStatus() {
-        return status;
-    }
-
-    public void setStatus(Integer status) {
-        this.status = status;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
         this.message = message;
     }
 }
