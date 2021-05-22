39065bf5acd60ebe3198ccb7ffedf1f24df5d5c1
samuelalmeida95 <samuelalmeida95>
2021-05-22 15:10:59 -0300

    adicionando um dto customizado para um ItemEmprestado, para alguns endpoint preciso que os dados do dono do item sejam mostrados, no restante dos endpoints não, por isso esse custom dto que trás um outro dto do dono a quem esse item pertence

---
+++ src/main/java/projects/givemebackapi/dtos/CustomAmigoEmprestimoDTO.java
@@ -0,0 +1,20 @@
+package projects.givemebackapi.dtos;
+
+import lombok.Data;
+import projects.givemebackapi.model.AmigoEmprestimo;
+
+@Data
+public class CustomAmigoEmprestimoDTO {
+
+    private String nome;
+    private String whatsApp;
+    private String endereco;
+
+    public  CustomAmigoEmprestimoDTO(AmigoEmprestimo amigo) {
+      this.nome = amigo.getNome();
+      this.whatsApp = amigo.getWhatsapp();  
+      this.endereco = amigo.getEndereco();
+    }
+
+    public CustomAmigoEmprestimoDTO(){}    
+}
