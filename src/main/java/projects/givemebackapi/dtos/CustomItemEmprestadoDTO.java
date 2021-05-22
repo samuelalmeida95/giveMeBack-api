819f0c43be27318000440af5cf9eaaea66934f15
samuelalmeida95 <samuelalmeida95>
2021-05-22 15:09:40 -0300

    adicionando um dto customizado para um ItemEmprestado, para alguns endpoint preciso que os dados do amigo que esta com o item emprestado sejam mostrados, no restante dos endpoints não, por isso esse custom dto que trás um outro dto do amigo que esta com posse do item

---
+++ src/main/java/projects/givemebackapi/dtos/CustomItemEmprestadoDTO.java
@@ -0,0 +1,29 @@
+package projects.givemebackapi.dtos;
+
+import java.time.LocalDate;
+
+import lombok.Data;
+import projects.givemebackapi.model.ItemEmprestado;
+import projects.givemebackapi.model.TipoStatus;
+
+@Data
+public class CustomItemEmprestadoDTO {
+    
+    private Integer id;
+    private String nome;
+    private String desc;
+    private LocalDate dataDevolucao;
+    private TipoStatus status;
+    private CustomAmigoEmprestimoDTO emprestado_para;
+
+    public CustomItemEmprestadoDTO(ItemEmprestado item) {
+       this.id = item.getIdItem();
+       this.nome = item.getNomeItem();
+       this.desc = item.getDescricaoItem();
+       this.dataDevolucao = item.getDataDevolucaoItem();
+       this.status = item.getStatus();
+       this.emprestado_para = new CustomAmigoEmprestimoDTO(item.getAmigoEmprestimo()); 
+    }
+
+    public CustomItemEmprestadoDTO() {}  
+}
