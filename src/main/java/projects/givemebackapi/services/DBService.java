463ea689f68b60b4ba01241a7c357c9b9030f1cd
samuelalmeida95 <samuelalmeida95>
2021-05-28 18:25:40 -0300

    Adicionando mais dados a base de dados automatica

--- src/main/java/projects/givemebackapi/services/DBService.java
+++ src/main/java/projects/givemebackapi/services/DBService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import projects.givemebackapi.model.AmigoEmprestimo;
+import projects.givemebackapi.model.AvaliacaoStatus;
 import projects.givemebackapi.model.DonoItem;
 import projects.givemebackapi.model.ItemEmprestado;
 import projects.givemebackapi.repositories.AmigoEmprestimoRepository;
@@ -32,8 +33,12 @@ public class DBService {
         DonoItem donoItem2 = new DonoItem(null, "Steve Jobs", "112-346-978");
     
         AmigoEmprestimo amigoEmprestimo = new AmigoEmprestimo(null, "José", "987", "rua do Jose");
-        AmigoEmprestimo amigoEmprestimo1 = new AmigoEmprestimo(null, "Pedro", "987", "rua do Pedro");
-        AmigoEmprestimo amigoEmprestimo2 = new AmigoEmprestimo(null, "João", "987", "avenida do João");
+        AmigoEmprestimo amigoEmprestimo1 = new AmigoEmprestimo(null, "Pedro", "123", "rua do Pedro");
+        AmigoEmprestimo amigoEmprestimo2 = new AmigoEmprestimo(null, "Marcos", "634", "avenida do Marcos");
+        AmigoEmprestimo amigoEmprestimo3 = new AmigoEmprestimo(null, "Flavio", "023", "avenida do Flavio");
+        AmigoEmprestimo amigoEmprestimo4 = new AmigoEmprestimo(null, "Alexa", "819", "avenida da Alexa");
+        AmigoEmprestimo amigoEmprestimo5 = new AmigoEmprestimo(null, "Ronaldo", "379", "avenida do Ronaldo");
+        AmigoEmprestimo amigoEmprestimo6 = new AmigoEmprestimo(null, "Robbin", "951", "rua do Robbin");
 
         ItemEmprestado itemEmprestado = new ItemEmprestado(null, "video game", "um video game", donoItem , amigoEmprestimo);
         ItemEmprestado itemEmprestado1 = new ItemEmprestado(null, "relogio", "um relogio", donoItem2 , amigoEmprestimo1);
@@ -43,14 +48,45 @@ public class DBService {
         ItemEmprestado itemEmprestado4 = new ItemEmprestado(null, "pipa", "uma pipa", donoItem1 , amigoEmprestimo2);
         ItemEmprestado itemEmprestado5 = new ItemEmprestado(null, "caneta", "uma caneta", donoItem1 , amigoEmprestimo2);
 
+        amigoEmprestimo.setAvaliacao(AvaliacaoStatus.OTIMA);
+        amigoEmprestimo1.setAvaliacao(AvaliacaoStatus.OTIMA);
+        amigoEmprestimo2.setAvaliacao(AvaliacaoStatus.BOA);
+        amigoEmprestimo3.setAvaliacao(AvaliacaoStatus.PESSIMA);
+        amigoEmprestimo4.setAvaliacao(AvaliacaoStatus.RUIM);
+        amigoEmprestimo5.setAvaliacao(AvaliacaoStatus.PESSIMA);
+        amigoEmprestimo6.setAvaliacao(AvaliacaoStatus.BOA);
 
-        donoItem.getItensEmprestados().addAll(Arrays.asList(itemEmprestado, itemEmprestado1, itemEmprestado2));
-       
-        amigoEmprestimoRepository.saveAll(Arrays.asList(amigoEmprestimo, amigoEmprestimo1, amigoEmprestimo2));
+        donoItem
+        .getItensEmprestados()
+        .addAll(Arrays
+        .asList(itemEmprestado, 
+                itemEmprestado1, 
+                itemEmprestado2));
 
-        donoItemRepositorie.saveAll(Arrays.asList(donoItem,donoItem1,donoItem2));
-        
-        itemEmprestadoRepositorie.saveAll(Arrays.asList(itemEmprestado, itemEmprestado1, itemEmprestado2, itemEmprestado3, itemEmprestado4, itemEmprestado5));
+        amigoEmprestimoRepository
+        .saveAll(Arrays
+        .asList(amigoEmprestimo, 
+               amigoEmprestimo1, 
+               amigoEmprestimo2,
+               amigoEmprestimo3, 
+               amigoEmprestimo4, 
+               amigoEmprestimo5, 
+               amigoEmprestimo6));
+
+        donoItemRepositorie
+        .saveAll(Arrays
+        .asList(donoItem, 
+               donoItem1, 
+               donoItem2));
+
+        itemEmprestadoRepositorie
+        .saveAll(Arrays
+        .asList(itemEmprestado, 
+                itemEmprestado1, 
+                itemEmprestado2,
+                itemEmprestado3, 
+                itemEmprestado4, 
+                itemEmprestado5));
        
         
     }
