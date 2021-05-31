a9e02e61e9adef7a8698c8ccdd0e58de46779703
samuelalmeida95 <samuelalmeida95>
2021-05-31 16:54:13 -0300

    removendo lombok

--- src/main/java/projects/givemebackapi/dtos/DonoItemDTO.java
+++ src/main/java/projects/givemebackapi/dtos/DonoItemDTO.java
@@ -1,9 +1,7 @@
 package projects.givemebackapi.dtos;
 
-import lombok.Data;
 import projects.givemebackapi.model.DonoItem;
 
-@Data
 public class DonoItemDTO {
 
   private Integer id;
@@ -17,4 +15,55 @@ public class DonoItemDTO {
   }
 
   public DonoItemDTO() {}
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getWhatsapp() {
+    return whatsapp;
+  }
+
+  public void setWhatsapp(String whatsapp) {
+    this.whatsapp = whatsapp;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    DonoItemDTO other = (DonoItemDTO) obj;
+    if (id == null) {
+      if (other.id != null)
+        return false;
+    } else if (!id.equals(other.id))
+      return false;
+    return true;
+  }
+
+  
 }
