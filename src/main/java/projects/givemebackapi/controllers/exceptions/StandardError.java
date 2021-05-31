2fdde867a7e85c7d50558514aba1b954c9d82a9c
samuelalmeida95 <samuelalmeida95>
2021-05-31 16:57:36 -0300

    removendo libs com conflito

--- src/main/java/projects/givemebackapi/controllers/exceptions/StandardError.java
+++ src/main/java/projects/givemebackapi/controllers/exceptions/StandardError.java
@@ -1,8 +1,5 @@
 package projects.givemebackapi.controllers.exceptions;
 
-import lombok.Data;
-
-@Data
 public class StandardError {
 
   private Long timestamp;
@@ -18,4 +15,55 @@ public class StandardError {
     this.status = status;
     this.message = message;
   }
+
+  public Long getTimestamp() {
+    return timestamp;
+  }
+
+  public void setTimestamp(Long timestamp) {
+    this.timestamp = timestamp;
+  }
+
+  public Integer getStatus() {
+    return status;
+  }
+
+  public void setStatus(Integer status) {
+    this.status = status;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((status == null) ? 0 : status.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    StandardError other = (StandardError) obj;
+    if (status == null) {
+      if (other.status != null)
+        return false;
+    } else if (!status.equals(other.status))
+      return false;
+    return true;
+  }
+
+  
 }
