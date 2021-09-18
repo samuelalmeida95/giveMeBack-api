3165f836a190794b148daf0252e4ec4ed1bb1196
samuelalmeida95 <samuka.ab.melo@hotmail.com>
2021-09-18 08:55:07 -0300

    ajustando controller de itens

--- src/main/java/projects/givemebackapi/controllers/ItemEmprestadoController.java
+++ src/main/java/projects/givemebackapi/controllers/ItemEmprestadoController.java
@@ -2,10 +2,11 @@ package projects.givemebackapi.controllers;
 
 import java.util.List;
 import java.util.stream.Collectors;
+
 import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,9 +17,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
 import projects.givemebackapi.dtos.CustomItemEmprestadoDTO;
 import projects.givemebackapi.dtos.ItemEmprestadoDTO;
-import projects.givemebackapi.model.AvaliacaoStatus;
 import projects.givemebackapi.model.ItemEmprestado;
 import projects.givemebackapi.model.TipoStatus;
 import projects.givemebackapi.services.ItemEmprestadoService;
@@ -98,7 +99,11 @@ public class ItemEmprestadoController {
 
   @PutMapping(value = "/alterar/{id}")
   @ResponseStatus(HttpStatus.OK)
-  public ItemEmprestadoDTO update(@PathVariable Integer id, @Valid @RequestBody ItemEmprestado item) {
+  public ItemEmprestadoDTO update(
+    @PathVariable Integer id, 
+    @Valid 
+    @RequestBody ItemEmprestado item) {
+      
     return new ItemEmprestadoDTO(service.update(id, item));
   }
 
